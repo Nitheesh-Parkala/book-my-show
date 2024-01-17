f7d5dea28a500a57572fce854ed4b563b42a3ad5
Nitheesh26 <nitheeshprabhu2001@gmail.com>
2024-01-17 22:29:13 +0530

    completed day3

--- src/pages/Home.Page.jsx
+++ src/pages/Home.Page.jsx
@@ -10,7 +10,7 @@ import HeroCarousel from '../components/HeroCarousel/HeroCarousel.Components'
 import PosterSlider from '../components/PosterSlider/PosterSlider.Components'
 
 
-const HomePage = () => {
+const HomePage = () => {  
  
   const [recommendedMovies, setrecommendedMovies]= useState([]);
   
@@ -22,16 +22,41 @@ const HomePage = () => {
     <>
       <HeroCarousel />
       <div className="container mx-auto px-4 md:px-12 my-8">
-        <h1 className = "text-2xl font-bold text-gray-800 sm:ml-3 my-3">
-        The Best Of Entertainment
+        <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 my-3">
+          The Best Of Entertainment
         </h1>
         <EntertainmentCardSlider />
       </div>
       <div className="container mx-auto px-4 md:px-12 my-8">
-
-        <PosterSlider  title= "Recommended Movies" 
+        <PosterSlider
+          title="Recommended Movies"
           subject="List of Recommended Movies"
-          posters= {recommendedMovies}
+          posters={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img
+              src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+              alt="Rupay"
+              className="w-full h-full"
+            />
+          </div>
+          <PosterSlider
+            title="premiers"
+            subject="Brand New release every Friday"
+            posters={premierMovies}
+            isDark={true}
+          />
+        </div>
+      </div>
+      <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+        <PosterSlider
+          title="Online Streaming Events"
+          subject="Online Streaming Events"
+          posters={onlineStreamEvents}
           isDark={false}
         />
       </div>
